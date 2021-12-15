277faaf171b304c5e8e5e1244276e011ee1497fd
HunterSl4d3 <modasserhasan@yahoo.com>
2021-12-15 23:25:38 +0600

    Update Tbomb.py

--- Tbomb.py
+++ Tbomb.py
@@ -28,17 +28,18 @@ def psb(z):
 
 def check_limit():
 	oj = str(op)
-	print("\033[92m	["+oj+"] Massage Sent Successfully.")
+	print("\033[92m    ["+oj+"] Massage Sent Successfully.")
 	if(op==tr):
-		psb("\n	[*] Bombing Finished Successfully..")
+		psb("\n    [*] Bombing Finished Successfully..")
 		time.sleep(0.4)
-		psb("	[!] Sent "+tr2+" Massages To 0"+number+" By ToxicBomber..\n\033[0;40;37m")
+		psb("    [!] Sent "+tr2+" Massages To 0"+number+" By ToxicBomber..\n\033[0;40;37m")
 		exit()
 		sys.exit()
 	else:
 		time.sleep(4)
         
 # Don't Delete Or Edit This LINE
+import base64
 exec(base64.b64decode('IyBFbmNvZGVkIEJ5IFB5RW5jcnlwdG9yCiMgQSBQcm9kdWN0IE9mIFRveGljTm9vYgojIGh0dHBzOi8vZ2l0aHViLmNvbS9Ub3hpYy1Ob29iCgppbXBvcnQgbWFyc2hhbApleGVjKG1hcnNoYWwubG9hZHMoYiJceGUzXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDZceDAwXHgwMFx4MDBAXHgwMFx4MDBceDAwc1x4ZmFceDAwXHgwMFx4MDBkXHgwMGRceDAxbFx4MDBaXHgwMGRceDAwZFx4MDFsXHgwMVpceDAxZFx4MDBkXHgwMWxceDAyWlx4MDJkXHgwMGRceDAxbFx4MDNaXHgwM2RceDAyZFx4MDNceDg0XHgwMFpceDA0ZFx4MDRkXHgwNVx4ODRceDAwWlx4MDVlXHgwMlx4YTBceDA2ZFx4MDZceGExXHgwMVx4MDFceDAwZVx4MDRkXHgwN1x4ODNceDAxXHgwMVx4MDBlXHgwMVx4YTBceDA3ZFx4MDhceGExXHgwMVx4MDFceDAwZVx4MDRkXHRceDgzXHgwMVx4MDFceDAwZVx4MDFceGEwXHgwN2Rcblx4YTFceDAxXHgwMVx4MDBlXHgwNWRceDBiXHg4M1x4MDFceDAxXHgwMGVceDA1ZFx4MGNceDgzXHgwMVx4MDFceDAwZVx4MDBceGEwXHgwOGRcclx4YTFceDAxWlx0ZVx0alxuWlx0elx4MDdlXHgwYmRceDBlZFx4MGZceDgzXHgwMlpceDBjV1x4MDBuLFx4MDFceDAwXHgwMVx4MDBceDAxXHgwMGVceDA1ZFx4MTBceDgzXHgwMVx4MDFceDAwZVx4MDVkXHgxMVx4ODNceDAxXHgwMVx4MDBlXHJkXHgxMlx4ODNceDAxWlx4MGVlXHgwNWRceDEzXHg4M1x4MDFceDAxXHgwMGVceDAyXHhhMFx4MDZkXHgxNFx4YTFceDAxXHgwMVx4MDBlXHgwMlx4YTBceDA2ZFx4MTVceGExXHgwMVx4MDFceDAwZVx4MDVkXHgxNlx4ODNceDAxXHgwMVx4MDBlXHgwMVx4YTBceDA3ZFx4MTdceGExXHgwMVx4MDFceDAwZVx4MDJceGEwXHgwNmRceDE4XHhhMVx4MDFceDAxXHgwMFlceDAwZVx4MGNceGEwXHgwZlx4YTFceDAwWlx4MGNlXHRaXHgxMGVceDBjWlx4MTFkXHgwMVNceDAwKVx4MTlceGU5XHgwMFx4MDBceDAwXHgwME5jXHgwMVx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMlx4MDBceDAwXHgwMFx4MDRceDAwXHgwMFx4MDBDXHgwMFx4MDBceDAwXHhmMzJceDAwXHgwMFx4MDB8XHgwMGRceDAxXHgxN1x4MDBEXHgwMF1ceDEyfVx4MDF0XHgwMGpceDAxXHhhMFx4MDJ8XHgwMVx4YTFceDAxXHgwMVx4MDB0XHgwMGpceDAxXHhhMFx4MDNceGExXHgwMFx4MDFceDAwdFx4MDRceGEwXHgwNWRceDAyXHhhMVx4MDFceDAxXHgwMHFceDA0ZFx4MDBTXHgwMClceDAzTlx4ZGFceDAxXG5nXHhmYX5qXHhiY3RceDkzaD9ceGE5XHgwNlx4ZGFceDAzc3lzXHhkYVx4MDZzdGRvdXRceGRhXHgwNXdyaXRlXHhkYVx4MDVmbHVzaFx4ZGFceDA0dGltZVx4ZGFceDA1c2xlZXBceGE5XHgwMlx4ZGFceDAxelx4ZGFceDAxZVx4YTlceDAwclx4MGVceDAwXHgwMFx4MDBceGRhXHgwMFx4ZGFceDA3cHNibG9nb1x4MDZceDAwXHgwMFx4MDBceGYzXG5ceDAwXHgwMFx4MDBceDBjXHgwMVx4MGNceDAxXG5ceDAxXHgwY1x4MDFceDA0XHhmZHJceDEwXHgwMFx4MDBceDAwY1x4MDFceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceDAwXHgwMFx4MDJceDAwXHgwMFx4MDBceDA0XHgwMFx4MDBceDAwQ1x4MDBceDAwXHgwMHJceDAyXHgwMFx4MDBceDAwKVx4MDNOclx4MDNceDAwXHgwMFx4MDBne1x4MTRceGFlR1x4ZTF6XHg5ND9yXHgwNFx4MDBceDAwXHgwMHJceDBiXHgwMFx4MDBceDAwclx4MGVceDAwXHgwMFx4MDByXHgwZVx4MDBceDAwXHgwMHJceDBmXHgwMFx4MDBceDAwXHhkYVx4MDZwc2Jsb2dceDBiXHgwMFx4MDBceDAwclx4MTFceDAwXHgwMFx4MDByXHgxMlx4MDBceDAwXHgwMFx4ZGFceDA1Y2xlYXJhXHgxZVx4MDFceDAwXHgwMFx4MWJbOTJtIF9fX19fICAgICAgICAgXyAgICAgIF8gICBfICAgICAgICAgICAgIF8gICAgIFxufF8gICBffF9fX18gIF8oXykgX19ffCBcXCB8IHwgX19fICAgX19fIHwgfF9fICBcbiAgfCB8LyBfIFxcIFxcLyAvIHwvIF9ffCAgXFx8IHwvIF8gXFwgLyBfIFxcfCAnXyBcXCBcbiAgfCB8IChfKSA+ICA8fCB8IChfX3wgfFxcICB8IChfKSB8IChfKSB8IHxfKSB8XG4gIHxffFxcX19fL18vXFxfXFxffFxcX19ffF98IFxcX3xcXF9fXy8gXFxfX18vfF8uX18vIFxuICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIGdceDAwXHgwMFx4MDBceDAwXHgwMFx4MDBceGUwP3orXHgxYlszOzkwbVx0XHQgICAgU2VjdXJpdHkgSXMgQW4gSWxsdXNpb25ceDFiWzA7OTJtZzMzMzMzM1x4ZTM/eipcbiAgICBbKl0gUGxlYXNlIENvbm5lY3QgVG8gVGhlIEludGVybmV0Li4uelx4MWEgICAgWypdIENoZWNraW5nIEFjY2Vzcy4uLnpKaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL1RveGljLU5vb2IvVG94aWNCb21iZXIvbWFpbi8udmVyc2lvbi50eHR6XHgwYy52ZXJzaW9uLnR4dFx4ZGFceDAxcno0XG4gICAgXHgxYlszMW1bIV0gVGhlcmUgSXMgYSBQcm9ibGVhbSB0byBTdGFydCBUaGUgVG9vbFxuejYgICAgXHgxYls5Mm1bKl0gWW91IEFyZSBBYm91dCBUbyBDbG9uZSBUaGlzIFRvb2wgQWdhaW4uLi56LVxuICAgIFshXSBQcmVzcyBFbnRlciBUbyBTdGFydCBDbG9uaW5nIFRvb2wuLi56IFxuICAgIFshXSBUb29sIENsb25pbmcgU3RhcnRlZC4uLnosY2QgLi4gJiYgcm0gLXJmIFRveGljQm9tYmVyID4gL2Rldi9udWxsIDI+JjF6TWNkIC4uICYmIGdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vVG94aWMtTm9vYi9Ub3hpY0JvbWJlciA+IC9kZXYvbnVsbCAyPiYxeixcbiAgICBbIV0gQ2xvbmluZyBDb21wbGV0ZWQsIFN0cmF0aW5nIFRvb2wuLi5ceGU5XHgwMVx4MDBceDAwXHgwMHoqY2QgLi4gJiYgY2QgVG94aWNCb21iZXIgJiYgcHl0aG9uIFRib21iLnB5KVx4MTJceGRhXHgwOHJlcXVlc3Rzclx0XHgwMFx4MDBceDAwXHhkYVx4MDJvc3JceDA1XHgwMFx4MDBceDAwclx4MTBceDAwXHgwMFx4MDByXHgxMlx4MDBceDAwXHgwMFx4ZGFceDA2c3lzdGVtclxuXHgwMFx4MDBceDAwXHhkYVx4MDNnZXRaXHgwN3Zlcm1haW5ceGRhXHgwNHRleHRceGRhXHgwNG9wZW5aXHgwNnZlcm5vd1x4ZGFceDA1aW5wdXRceGRhXHgwMm9wXHhkYVx4MDRyZWFkWlxyc2VjdXJpdHlfY29kZVpceDBjc2VjdXJpdHlfSzNZclx4MGVceDAwXHgwMFx4MDByXHgwZVx4MDBceDAwXHgwMHJceDBlXHgwMFx4MDBceDAwclx4MGZceDAwXHgwMFx4MDBceGRhXHgwODxtb2R1bGU+XHgwMVx4MDBceDAwXHgwMHM8XHgwMFx4MDBceDAwXHgwOFx4MDBceDA4XHgwMVx4MDhceDAxXHgwOFx4MDFceDA4XHgwMlx4MDhceDA1XG5ceDA2XHgwOFx4MDFcblx4MDFceDA4XHgwMVxuXHgwMVx4MDhceDAxXHgwOFx4MDFcblx4MDFceDA2XHgwMVx4MDJceDAxXHgwZVx4MDFceDA2XHgwMVx4MDhceDAxXHgwOFx4MDFceDA4XHgwMVx4MDhceDAxXG5ceDAxXG5ceDAxXHgwOFx4MDFcblx4MDFceDBjXHgwMVx4MDhceDAxXHgwNFx4MDFceDA4XHgwMSIpKQ=='))
 
 br = mechanize.Browser()
@@ -46,7 +47,7 @@ br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
 if(security_code==security_K3Y):
-	psb("\n\t[!] Access Approved.....")
+	psb("\n    [!] Access Approved.....")
 	time.sleep(0.6)
 	
 	
@@ -61,14 +62,14 @@ if(security_code==security_K3Y):
 	
 	
 	print("\033[92m")
-	number = input("\n	[*] Enter Target Number:> ").replace("+880", "").replace("880", "")
+	number = input("\n    [*] Enter Target Number:> ").replace("+880", "").replace("880", "")
 	if (number[0] == "0"):
 	    number = number[-10:]
-	tr = input("	[*] Enter Amount (Default 10):> ")
+	tr = input("    [*] Enter Amount (Default 10):> ")
 	if(tr==""):
 		tr=10
 	tr = int(tr)
-	psb("	[*] Auto Delay: 4 Seconds...")
+	psb("    [*] Auto Delay: 4 Seconds...")
 	time.sleep(0.5)
 	
 	num = "880"+number
@@ -81,9 +82,9 @@ if(security_code==security_K3Y):
 	data={"phone":{"number":num3,"internationalNumber":"+880 "+num3,"nationalNumber":"0"+num3,"e164Number":"+880"+num3,"countryCode":"BD","dialCode":"+880"},"contact":num,"type":"phone"}
 	
 	
-	psb("\n	[!] Please wait.....")
+	psb("\n    [!] Please wait.....")
 	time.sleep(0.6)
-	psb("	[!] Bombing in Progress....\n\n")
+	psb("    [!] Bombing in Progress....\n\n")
 	time.sleep(1)
 	
 	op = 0
@@ -104,7 +105,7 @@ if(security_code==security_K3Y):
 					op = op + 1
 					check_limit()
 				else:
-					print("\033[31m	[!] Massage Send Failed\n")
+					print("\033[31m    [!] Massage Send Failed\n")
 					time.sleep(4)
 					
 		if(op==tr):
@@ -116,7 +117,7 @@ if(security_code==security_K3Y):
 		        op=op+1
 		        check_limit()
 		    else:
-		        print("\033[31m	[!] Massege Send Failed!\n")
+		        print("\033[31m    [!] Massege Send Failed!\n")
 		        time.sleep(4)
 		except:
 			time.sleep(4)
@@ -135,7 +136,7 @@ if(security_code==security_K3Y):
 		    		op=op+1
 		    		check_limit()
 		    	else:
-		    		print("\033[31m	[!] Massage Send Failed!\n")
+		    		print("\033[31m    [!] Massage Send Failed!\n")
 		    		time.sleep(4)
 		except:
 			time.sleep(4)
@@ -149,7 +150,7 @@ if(security_code==security_K3Y):
 				op=op+1
 				check_limit()
 			else:
-				print("\033[31m	[!] Massage Send Failed!\n")
+				print("\033[31m    [!] Massage Send Failed!\n")
 				time.sleep(4)
 		except:
 			time.sleep(4)
@@ -163,7 +164,7 @@ if(security_code==security_K3Y):
 				op=op+1
 				check_limit()
 			else:
-				print("\033[31m	[!] Massage Send Failed!")
+				print("\033[31m    [!] Massage Send Failed!")
 				time.sleep(4)
 		except:
 			time.sleep(4)
@@ -176,7 +177,7 @@ if(security_code==security_K3Y):
 			op=op+1
 			check_limit()
 		except:
-			print("\033[31m	[!] Massage Send Failed!")
+			print("\033[31m    [!] Massage Send Failed!")
 			time.sleep(4)
 
 		if(op==tr):
@@ -188,7 +189,7 @@ if(security_code==security_K3Y):
 				op=op+1
 				check_limit()
 			else:
-				print("\033[31m	[!] Massage Send Failed!")
+				print("\033[31m    [!] Massage Send Failed!")
 				time.sleep(4)
 		except:
 			time.sleep(4)
@@ -202,10 +203,9 @@ if(security_code==security_K3Y):
 				op=op+1
 				check_limit()
 			else:
-				print("\033[31m	[!] Massage Send Failed!")
+				print("\033[31m    [!] Massage Send Failed!")
 				time.sleep(4)
 		except:
-			print("\033[31m	[!0] Massage Send Failed!")
 			time.sleep(4)
 			
 		if(toxic.fun(number)):
@@ -222,4 +222,4 @@ if(security_code==security_K3Y):
 
 else:
 # Don't Delete Or Edit This LINE
-	exec(base64.b64decode('cHNibG9nKCJcblx0WyFdIFRvb2wgVXBkYXRlIEZvdW5kLi4uIikscHNibG9nKCJcblx0WyFdIFVwZGF0aW5nIFRvb2wuLi5cMDMzWzBtIiksb3Muc3lzdGVtKCJjZCAuLiAmJiBybSAtcmYgVG94aWNCb21iZXIgJiYgZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9Ub3hpYy1Ob29iL1RveGljQm9tYmVyID4gL2Rldi9udWxsIDI+JjEgJiYgY2QgVG94aWNCb21iZXIgJiYgcHl0aG9uIFRib21iLnB5IikK'))
+	exec(base64.b64decode('cHNibG9nKCJcbiAgICBbIV0gVG9vbCBVcGRhdGUgRm91bmQuLi4iKSxwc2Jsb2coIiAgICBbIV0gVXBkYXRpbmcgVG9vbC4uLiIpLG9zLnN5c3RlbSgiY2QgLi4gJiYgcm0gLXJmIFRveGljQm9tYmVyID4gL2Rldi9udWxsIDI+JjEgJiYgZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9Ub3hpYy1Ob29iL1RveGljQm9tYmVyID4gL2Rldi9udWxsIDI+JjEiKSxwc2Jsb2coIiAgICBbKl0gVXBkYXRlIENvbXBsZXRlZCwgU3RydGluZyBUb29sLi4iKSx0aW1lLnNsZWVwKDEpLG9zLnN5c3RlbSgiY2QgLi4gJiYgY2QgVG94aWNCb21iZXIgJiYgcHl0aG9uIFRib21iLnB5IikK'))
